12b20a57036c2da73bd55a313de215297a48fc37
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2018-02-09 16:12:41 +0700

    Add HomeActivity

--- app/src/main/java/id/noidea/printin/SplashActivity.java
+++ app/src/main/java/id/noidea/printin/SplashActivity.java
@@ -4,11 +4,12 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIMEOUT = 3000;
+    private static int SPLASH_TIMEOUT = 1000;
     private ProgressBar progressBar;
 
     @Override
@@ -18,10 +19,13 @@ public class SplashActivity extends AppCompatActivity {
 
         progressBar = findViewById(R.id.progressBar);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
             }
